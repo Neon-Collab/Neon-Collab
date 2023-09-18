6ef5dae6174a8455dd1059c5d3028bfb6036d8e1
LLaxt <laurenlaxton1@gmail.com>
2023-09-18 11:51:11 -0700

    add form components and conditional form rendering

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import LoginForm from '../components/LoginComponents/LoginForm.jsx';
 import SignupForm from '../components/LoginComponents/SignupForm.jsx';
 
 // save skill level in postgres and auth data in firestore
 
 export default function LoginPage() {
+  const [newUser, setNewUser] = useState(false);
   return (
     <div>
       <h1>Login Page</h1>
-      <LoginForm />
+      { newUser ? <SignupForm /> : <LoginForm /> }
+      <input
+        type="button"
+        onClick={() => setNewUser(!newUser)}
+        value={newUser ? 'Already a member? Log In' : 'New to NeonCollab? Create an Account'}
+      />
     </div>
   );
 }
