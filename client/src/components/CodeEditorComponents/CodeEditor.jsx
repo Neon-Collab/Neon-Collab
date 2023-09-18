ef2b655beb0503937b8b25e09582c665bd169091
Eric Chang <fsc8504@gmail.com>
2023-09-17 22:00:51 -0700

    Add submission

--- client/src/components/CodeEditorComponents/CodeEditor.jsx
+++ client/src/components/CodeEditorComponents/CodeEditor.jsx
@@ -5,12 +5,15 @@ import 'ace-builds/src-noconflict/mode-javascript';
 import 'ace-builds/src-noconflict/theme-monokai';
 
 function CodeEditor({ value, onChange }) {
+
   return (
     <AceEditor
       mode="javascript"
       theme="monokai"
       onChange={onChange}
+      value={value}
       name="neon_code_editor"
+      highlightActiveLine
       editorProps={{ $blockScrolling: true }}
     />
   );
--- client/src/pages/CodeEditorPage.jsx
+++ client/src/pages/CodeEditorPage.jsx
@@ -1,16 +1,54 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import CodeEditor from '../components/CodeEditorComponents/CodeEditor.jsx';
 import SelectedProblem from '../components/CodeEditorComponents/SelectedProblem.jsx';
 
 function CodeEditorPage() {
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState('// Write your JavaScript code here...');
+  const submitCode = () => {
+    if (window.confirm('Would you like to submit your code?')) {
+      // For testing only
+      const userId = 1;
+      const problemId = 1;
+
+      axios.post('/codeEditor/submit', {
+        userId,
+        problemId,
+        code,
+      })
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    } else {
+      console.log('Code submission canceled.');
+    }
+  };
   return (
     <div>
-      <SelectedProblem />
-      <CodeEditor value={code} onChange={setCode} />
-      <button>Submit</button>
+      <div style={containerStyle}>
+        <div style={problemStyle}>
+          <SelectedProblem />
+        </div>
+        <div>
+          <CodeEditor value={code} onChange={setCode} />
+          <button type="button" onClick={submitCode}>Submit</button>
+        </div>
+      </div>
     </div>
   );
 }
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'flex-start',
+};
+
+const problemStyle = {
+  marginRight: '20px',
+};
 
 export default CodeEditorPage;
