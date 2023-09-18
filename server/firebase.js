72bc22e6d55198211c7ae0f8fab45ee64693573b
LLaxt <laurenlaxton1@gmail.com>
2023-09-18 00:27:07 -0700

    update register params and export fns

--- server/firebase.js
+++ server/firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from 'firebase/app';
-//import { getAnalytics } from 'firebase/analytics';
+// import { getAnalytics } from 'firebase/analytics';
 
 import {
-  GoogleAuthProvider,
+  // GoogleAuthProvider,
   getAuth,
   signInWithEmailAndPassword,
-  signInWithPopup,
+  // signInWithPopup,
   createUserWithEmailAndPassword,
   sendPasswordResetEmail,
   signOut,
@@ -17,12 +17,21 @@ import {
   getDocs,
   collection,
   where,
-  adDoc,
+  addDoc,
 } from 'firebase/firestore';
 
+/*
+"Unlike how API keys are typically used, API keys for Firebase services
+are not used to control access to backend resources; that can only be
+done with Firebase Security Rules (to control which users can access
+resources) and App Check (to control which apps can access resources).
+Usually, you need to fastidiously guard API keys (for example, by using
+a vault service or setting the keys as environment variables); however,
+API keys for Firebase services are ok to include in code or checked-in config files."
+*/
 
-//"Unlike how API keys are typically used, API keys for Firebase services are not used to control access to backend resources; that can only be done with Firebase Security Rules (to control which users can access resources) and App Check (to control which apps can access resources). Usually, you need to fastidiously guard API keys (for example, by using a vault service or setting the keys as environment variables); however, API keys for Firebase services are ok to include in code or checked-in config files.""
-//tl;dr: Firebase config is ok to include in checked-in code. Docs: https://firebase.google.com/docs/projects/api-keys#api-keys-for-firebase-are-different
+// tl;dr: Firebase config is ok to include in checked-in code.
+// Docs: https://firebase.google.com/docs/projects/api-keys#api-keys-for-firebase-are-different
 const firebaseConfig = {
   apiKey: 'AIzaSyD76PMQ7QemBetVTdn-YIPMgEoevFv3zw4',
   authDomain: 'neon-collab.firebaseapp.com',
@@ -30,14 +39,14 @@ const firebaseConfig = {
   storageBucket: 'neon-collab.appspot.com',
   messagingSenderId: '416675025384',
   appId: '1:416675025384:web:e64c7111fc556894a6545d',
-  measurementId: 'G-YN7ZN7ZRH7'
+  measurementId: 'G-YN7ZN7ZRH7',
 };
 
 const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
 
 const auth = getAuth(app);
-​​const db = getFirestore(app);
+const db = getFirestore(app);
 
 const logInWithEmailAndPassword = async (email, password) => {
   try {
@@ -48,13 +57,16 @@ const logInWithEmailAndPassword = async (email, password) => {
   }
 };
 
-const registerWithEmailAndPassword = async (name, email, password) => {
+const registerWithEmailAndPassword = async (firstname, lastname, username, skill, email, password) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
-    const user = res.user;
+    const { user } = res;
     await addDoc(collection(db, 'users'), {
       uid: user.uid,
-      name,
+      firstname,
+      lastname,
+      username,
+      skill,
       authProvider: 'local',
       email,
     });
@@ -62,4 +74,27 @@ const registerWithEmailAndPassword = async (name, email, password) => {
     console.error(err);
     alert(err.message);
   }
+};
+
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert('Password reset link sent!');
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export {
+  auth,
+  db,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordReset,
+  logout,
 };
