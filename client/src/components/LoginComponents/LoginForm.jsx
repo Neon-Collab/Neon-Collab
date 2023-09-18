197870a319b26fd3378227f6f33085ffff50b4d2
LLaxt <laurenlaxton1@gmail.com>
2023-09-18 11:51:11 -0700

    add form components and conditional form rendering

--- client/src/components/LoginComponents/LoginForm.jsx
+++ client/src/components/LoginComponents/LoginForm.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { auth, signInWithEmailAndPasword } from '../../../../server/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 export default function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <form onSubmit={() => { console.log('Form Submitted'); }}>
-      <input type="text" placeholder="Username" />
+      <input type="email" placeholder="email" />
       <input type="text" placeholder="Password" />
       <input type="submit" value="Log In" />
     </form>
--- client/src/components/LoginComponents/SignupForm.jsx
+++ client/src/components/LoginComponents/SignupForm.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SignupForm() {
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [username, setUsername]= useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  //add skill level
   return (
-    <form>
-      <input type="text" placeholder="your info here..." />
+    <form onSubmit={() => { console.log('Form Submitted'); }}>
+      <input type="text" placeholder="First Name" />
+      <input type="text" placeholder="Last Name" />
+      <input type="text" placeholder="Username" />
+      <input type="email" placeholder="Email" />
+      <input type="text" placeholder="Password" />
+      <input type="text" placeholder="Verify Password" />
+      <input type="submit" value="Create Account" />
     </form>
   );
 }
