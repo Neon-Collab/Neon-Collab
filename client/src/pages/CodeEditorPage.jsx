2ff88f50a0bddb4a698ebd62ed68d4bf5ed931de
Eric Chang <fsc8504@gmail.com>
2023-09-17 17:37:50 -0700

    Update files

--- client/src/pages/CodeEditorPage.jsx
+++ client/src/pages/CodeEditorPage.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CodeEditor from '../components/CodeEditorComponents/CodeEditor.jsx';
 import SelectedProblem from '../components/CodeEditorComponents/SelectedProblem.jsx';
 
 function CodeEditorPage() {
+  const [code, setCode] = useState('');
   return (
     <div>
       <SelectedProblem />
-      <CodeEditor />
+      <CodeEditor value={code} onChange={setCode} />
       <button>Submit</button>
     </div>
   );
