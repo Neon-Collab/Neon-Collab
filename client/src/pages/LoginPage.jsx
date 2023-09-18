8c12e12e1425c75348093277c39e379fdde7272c
LLaxt <laurenlaxton1@gmail.com>
2023-09-18 00:35:53 -0700

    exclude skill from auth db, to be saved in postgres

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import LoginForm from '../components/LoginComponents/LoginForm.jsx';
 import SignupForm from '../components/LoginComponents/SignupForm.jsx';
 
+// save skill level in postgres and auth data in firestore
+
 export default function LoginPage() {
   return (
     <div>
