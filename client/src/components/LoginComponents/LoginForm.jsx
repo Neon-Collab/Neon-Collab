5ce787952a5421b3a7cff92b65ff388712c7c2ae
LLaxt <laurenlaxton1@gmail.com>
2023-09-18 15:26:14 -0700

    update form components

--- client/src/components/LoginComponents/LoginForm.jsx
+++ client/src/components/LoginComponents/LoginForm.jsx
@@ -1,15 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import { auth, signInWithEmailAndPasword } from '../../../../server/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import {
+  auth,
+  signInWithEmailAndPasword
+} from '../../../../server/firebase';
+import {
+  logInWithEmailAndPassword,
+} from '../../../../server/controllers/firebaseController';
 
 export default function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const submitLoginInfo = () => {
+    // prevent default?
+    logInWithEmailAndPassword(email, password);
+    // add navigation if successful
+  };
+
   return (
-    <form onSubmit={() => { console.log('Form Submitted'); }}>
-      <input type="email" placeholder="email" />
-      <input type="text" placeholder="Password" />
+    <form onSubmit={submitLoginInfo}>
+      <input
+        type="email"
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <input type="submit" value="Log In" />
     </form>
   );
--- client/src/components/LoginComponents/SignupForm.jsx
+++ client/src/components/LoginComponents/SignupForm.jsx
@@ -1,4 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {
+  auth,
+  signInWithEmailAndPasword
+} from '../../../../server/firebase';
+import {
+  registerWithEmailAndPassword,
+} from '../../../../server/controllers/firebaseController';
 
 export default function SignupForm() {
   const [firstname, setFirstname] = useState('');
@@ -6,16 +14,34 @@ export default function SignupForm() {
   const [username, setUsername]= useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [skill, setSkill] = useState('');
 
-  //add skill level
+  const submitLoginInfo = () => {
+    // prevent default?
+    logInWithEmailAndPassword(email, password);
+    // add navigation if successful
+  };
+
+  // add skill level/submit username/email/skill
   return (
     <form onSubmit={() => { console.log('Form Submitted'); }}>
-      <input type="text" placeholder="First Name" />
-      <input type="text" placeholder="Last Name" />
-      <input type="text" placeholder="Username" />
-      <input type="email" placeholder="Email" />
-      <input type="text" placeholder="Password" />
-      <input type="text" placeholder="Verify Password" />
+      <input type="text" placeholder="First Name" required />
+      <input type="text" placeholder="Last Name" required />
+      <input type="text" placeholder="Username" required />
+      <input type="email" placeholder="Email" required />
+      <input type="text" placeholder="Password" required />
+      <input type="text" placeholder="Verify Password" required />
+      <select
+        value={skill}
+        onChange={(e) => e.target.value}
+        required
+      >
+        <option value="">Select your level of experience with JavaScript...</option>
+        <option value="Beginner">Beginer</option>
+        <option value="Intermediate">Intermediate</option>
+        <option value="Advanced">Advanced</option>
+        <option value="Expert">Expert</option>
+      </select>
       <input type="submit" value="Create Account" />
     </form>
   );
