ef95c5d6cdfc0ead65b76d0f9516805822f39ef1
Patrick Alexandre <palexandre822@gmail.com>
2023-09-21 14:10:25 -0700

    fetch data at top level profile component and pass down props to children

--- client/src/components/ProfilePageComponents/AccountInfo.jsx
+++ client/src/components/ProfilePageComponents/AccountInfo.jsx
@@ -2,16 +2,9 @@ import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import AppContext from '../../contexts/AppContext.jsx';
 
-export default function AccountInfo() {
+export default function AccountInfo({ user }) {
   const { account, setAccount } = useContext(AppContext);
-  const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    axios.get(`/api/users/search/${account.username}`)
-      .then((response) => {
-        setUser(response.data[0]);
-      });
-  }, []);
   return (
     <div className="component-container">
       <h1>Account Information</h1>
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,18 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import styles from '../components/ProfilePageComponents/styles.css';
 import AccountInfo from '../components/ProfilePageComponents/AccountInfo';
 import ProblemHistory from '../components/ProfilePageComponents/ProblemHistory';
 import Ranking from '../components/ProfilePageComponents/Ranking';
 import ProblemsSolved from '../components/ProfilePageComponents/ProblemsSolved';
+import AppContext from '../contexts/AppContext.jsx';
 
 export default function ProfilePage() {
+  // Move useEffect to get user data of current logged in user here
+  // Pass down user_id to Ranking and username to AccountInfo
+  const { account, setAccount } = useContext(AppContext);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    axios.get(`/api/users/search/${account.username}`)
+      .then((response) => {
+        setUser(response.data[0]);
+      });
+  }, []);
   return (
     <div className="profile-container">
-      <AccountInfo />
-      <Ranking />
-      <ProblemHistory />
-      <ProblemsSolved />
+      {user ? (
+        <>
+          <AccountInfo user={user} />
+          <Ranking id={user.user_id} />
+          <ProblemHistory />
+          <ProblemsSolved />
+        </>
+      ) : <h1>Loading...</h1>}
     </div>
   );
 }
