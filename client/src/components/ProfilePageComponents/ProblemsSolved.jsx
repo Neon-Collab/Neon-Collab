7d05ce4836354e7c1851b1f4d25cb2ee39e64227
Patrick Alexandre <palexandre822@gmail.com>
2023-09-21 16:10:26 -0700

    styling and changed problems solved to problems submitted

--- client/src/components/ProfilePageComponents/ProblemsSolved.jsx
+++ client/src/components/ProfilePageComponents/ProblemsSolved.jsx
@@ -12,7 +12,7 @@ export default function ProblemsSolved({ id }) {
   }, []);
   return (
     <div className="common-container component-container">
-      <h1>Problems Solved</h1>
+      <h1>Problems Submitted</h1>
       <h2>{solved}</h2>
     </div>
   );
