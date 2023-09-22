8a2cbfd4b15c8edc686f7c9802467ee6b3238dd0
LLaxt <laurenlaxton1@gmail.com>
2023-09-21 21:50:40 -0700

    switch table styles

--- client/src/components/ProblemPageComponents/Leaderboard.jsx
+++ client/src/components/ProblemPageComponents/Leaderboard.jsx
@@ -27,12 +27,11 @@ function Leaderboard() {
         console.error(error);
       });
   }, []);
-  //  I also created table styles - feel free to try them or keep yours!
-  // <table className="rank-table">
+
   return (
     <div className="common-container">
       <h2 style={{ color: 'magenta', textAlign: 'center' }}>Leaderboard</h2>
-      <table style={tableStyles}>
+      <table className="rank-table">
         <thead>
           <tr>
             <th style={thTdStyles}>Rank</th>
--- client/src/components/ProblemPageComponents/RankingbyProblem.jsx
+++ client/src/components/ProblemPageComponents/RankingbyProblem.jsx
@@ -40,12 +40,11 @@ function RankingbyProblem({ problemId }) {
         });
     }
   }, [problemId]);
-  //  I also created table styles - feel free to try them or keep yours!
-  // <table className="rank-table">
+
   return (
     <div className="common-container">
       <h2 style={{ color: 'magenta', textAlign: 'center' }}>Ranking for Problem: {problemName}</h2>
-      <table style={tableStyles}>
+      <table className="rank-table">
         <thead>
           <tr>
             <th style={thTdStyles}>Rank</th>
